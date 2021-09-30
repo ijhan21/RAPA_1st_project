383de0c8f34e586ef8088fb157cbd51fde0b8c01
PC025 <reinforcehan@gmail.com>
2021-09-30 15:24:44 +0900

    stable version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from utils import *
+from utils_normal7 import *
 import time
 import cv2
 
@@ -8,31 +8,33 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 get_data = GetData()
 motion_detect = Motion_Detect()
-tm = cv2.TickMeter()
-time_checker = list()
+actionInstance = Action()
+title = 'webcam'
 while True:
-    # 시간 측정
-    tm.reset()
-    tm.start()
     ret, frame = cap.read()
     if ret:
         # 이미지 분석해서 손동작과 손목 좌표를 받아온다
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        state, coordinate, frame = get_data.state_update(frame)
+        state, coordinate = get_data.state_update(frame)
         # print(state, coordinate)
         # 손동작과 손목 좌표를 통해서 모션을 해석
-        action = motion_detect.motion_to_action(state, coordinate)
-        if action is not None:
-            print(action)
+        action, highlight = motion_detect.motion_to_action(state, coordinate)
+        actionInstance.ReceiveFrame(frame)
+        if highlight == False :            
+            actionInstance.action(act='slideshow',direction=action)
+        else:  
+            actionInstance.action(act='spotlight',direction=action)
+        
 
-        cv2.imshow('frame', frame)
-        key = cv2.waitKey(1)
-        if key ==27:
-            break        
-    else:
-        print(frame)
-    tm.stop()
-    ms = tm.getTimeMilli()
-    time_checker.append(ms)
-print("평균 처리 시간:",round(np.mean(np.array(time_checker)),2))
+        print(f'action : {action}, highlight : {highlight}')
+        cv2.putText(frame,str(action),(100,100),1,3,(255,100,0),2)
+    
+
+    # cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)  
+    # cv2.setWindowProperty(title, cv2.WND_PROP_TOPMOST, 1)
+    # cv2.imshow(title, frame)
+    key = cv2.waitKey(20)
+    if key ==27:
+        break
+    # else:
+    #     print(frame)
 
